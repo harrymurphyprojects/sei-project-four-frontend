6969021cd3a51742ea8fc7ea604a35c8af57c092
harrymurphy1534 <harryjmurphy1997@gmail.com>
2022-03-10 01:16:56 +0000

    README TEST TWO

--- src/lib/api.js
+++ src/lib/api.js
@@ -16,13 +16,13 @@ export function getSingleJob(jobId) {
   return axios.get(`${baseUrl}/jobs/${jobId}`)
 }
 
-// export function createJob(formData) {
-//   return axios.post('/api/jobs/', formData, headers())
-// }
+export function createJob(formData) {
+  return axios.post('/api/jobs/', formData, headers())
+}
 
-// export function deleteJob(jobId) {
-//   return axios.delete(`/api/jobs/${jobId}`, headers())
-// }
+export function deleteJob(jobId) {
+  return axios.delete(`/api/jobs/${jobId}`, headers())
+}
 
 export function register(formData) {
   return axios.post(`${baseUrl}/register/`, formData)
